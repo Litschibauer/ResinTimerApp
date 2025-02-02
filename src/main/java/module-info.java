c071f0778640562eea4f278b54feb708222eea7d
Litschibauer <tobias@wellner.ch>
2025-02-03 00:09:51 +0100

    fix

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module de.litschibauer.harztimerapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-
-
-    opens de.litschibauer.harztimerapp to javafx.fxml;
-    exports de.litschibauer.harztimerapp;
-}
